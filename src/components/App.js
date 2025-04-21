f343bcde5a6b02d8bc349780fd374341b9da17d8
abdiaziz77 <abdiaziznoor20@gmail.com>
2025-04-21 15:53:02 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
 import ProjectList from "./ProjectList";
 import user from "../data/user";
 
@@ -9,8 +7,6 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
       <ProjectList projects={user.projects} />
     </div>
   );
